8ca7256fe358aa996126bc04102100b87bb22b62
n-caffa <55499184+n-caffa@users.noreply.github.com>
2026-01-13 20:09:26 +0100

    Chore: add hexagonal package structure and base configuration

--- src/main/java/com/inditex/pricing/PricingServiceApplication.java
+++ src/main/java/com/inditex/pricing/PricingServiceApplication.java
@@ -1,4 +1,4 @@
-package com.inditex.pricing.pricing_service;
+package com.inditex.pricing;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/test/java/com/inditex/pricing/PricingServiceApplicationTests.java
+++ src/test/java/com/inditex/pricing/PricingServiceApplicationTests.java
@@ -1,4 +1,4 @@
-package com.inditex.pricing.pricing_service;
+package com.inditex.pricing;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
